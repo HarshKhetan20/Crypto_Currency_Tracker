88c91057e569d0727dc32aea839e8dc0ce529695
Harsh Khetan <harshkhetan050@gmail.com>
2025-07-15 13:57:28 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey: "AIzaSyDuYa9g5U_mfGFKP9ro9_az9NgDKpkWpRA",
-  authDomain: "cryptocurrency-tracker-9cdb2.firebaseapp.com",
-  projectId: "cryptocurrency-tracker-9cdb2",
-  storageBucket: "cryptocurrency-tracker-9cdb2.appspot.com",
-  messagingSenderId: "207586720773",
-  appId: "1:207586720773:web:9de1790acad858226c306c",
-  measurementId: "G-KJ79XNES62"
+  apiKey: "FIREBASE CONFIG_API_KEY",
+  authDomain: "FIREBASE AUTH DOMAIN",
+  projectId: "FIREBASE PROJECT_ID",
+  storageBucket: "FIREBASE STORAGE_BUCKET",
+  messagingSenderId: "FIREBASE MESSAGING_SENDERID",
+  appId: "FIREBASE APP_ID",
+  measurementId: "FIREBASE MEASUREMENTID"
 };
 
 // Initialize Firebase
